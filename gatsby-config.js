c11ff9d085d16155cd41ab6936b3b593afa0a96e
nidhi.gokani <gokaninidhi9521@gmail.com>
2022-10-07 10:12:03 +0530

    commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,14 +23,13 @@ module.exports = {
         ],
       },
     },
+    `gatsby-plugin-image`,
     `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-source-contentful`,
       options: {
-        spaceId: process.env.CONTENTFUL_SPACE_ID || "93ikzaev9eog",
-        accessToken:
-          process.env.CONTENTFUL_ACCESS_TOKEN ||
-          "L3Z7MHgYHXy0D7KXHd_JGWNssTaFJNqlqxlkGnSt02c",
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
         host: process.env.CONTENTFUL_HOST,
       },
     },
@@ -53,14 +52,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, 
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
   ],
